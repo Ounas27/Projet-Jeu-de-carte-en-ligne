f6c2212abe665b5870018ea5c0b39b12a0e7200f
Ounas27 <73233838+Ounas27@users.noreply.github.com>
2022-03-10 11:12:19 +0100

    Add files via upload

--- accueil.php
+++ accueil.php
@@ -9,35 +9,35 @@
     <body>
         <?php
             echo"<div id=setJeux1>
-                    <div id=carte1><img src='./CartesAJouer/1_11.png'></div>
-                    <div id=carte2><img src='./CartesAJouer/2_11.png'></div>
-                    <div id=carte3><img src='./CartesAJouer/3_11.png'></div>
-                    <div id=carte4><img src='./CartesAJouer/4_11.png'></div>
-                    <div id=carte5><img src='./CartesAJouer/1_9.png'></div>
+                    <div id=carte1><img src='./ImagesCartes/1_11.png'></div>
+                    <div id=carte2><img src='./ImagesCartes/2_11.png'></div>
+                    <div id=carte3><img src='./ImagesCartes/3_11.png'></div>
+                    <div id=carte4><img src='./ImagesCartes/4_11.png'></div>
+                    <div id=carte5><img src='./ImagesCartes/1_9.png'></div>
                 </div>
 
-                <div id=setJeux2>
-                    <div id=carte1><img src='./CartesAJouer/0_3.png'></div>
-                    <div id=carte2><img src='./CartesAJouer/0_3.png'></div>
-                    <div id=carte3><img src='./CartesAJouer/0_3.png'></div>
-                    <div id=carte4><img src='./CartesAJouer/0_3.png'></div>
-                    <div id=carte5><img src='./CartesAJouer/0_3.png'></div>
+                <<div id=setJeux2>
+                    <div id=carte1><img src='./ImagesCartes/0_3.png'></div>
+                    <div id=carte2><img src='./ImagesCartes/0_3.png'></div>
+                    <div id=carte3><img src='./ImagesCartes/0_3.png'></div>
+                    <div id=carte4><img src='./ImagesCartes/0_3.png'></div>
+                    <div id=carte5><img src='./ImagesCartes/0_3.png'></div>
                 </div>
 
                 <div id=setJeux3>
-                    <div id=carte1><img src='./CartesAJouer/0_3.png'></div>
-                    <div id=carte2><img src='./CartesAJouer/0_3.png'></div>
-                    <div id=carte3><img src='./CartesAJouer/0_3.png'></div>
-                    <div id=carte4><img src='./CartesAJouer/0_3.png'></div>
-                    <div id=carte5><img src='./CartesAJouer/0_3.png'></div>
+                    <div id=carte1><img src='./ImagesCartes/0_3.png'></div>
+                    <div id=carte2><img src='./ImagesCartes/0_3.png'></div>
+                    <div id=carte3><img src='./ImagesCartes/0_3.png'></div>
+                    <div id=carte4><img src='./ImagesCartes/0_3.png'></div>
+                    <div id=carte5><img src='./ImagesCartes/0_3.png'></div>
                 </div>
 
                 <div id=setJeux4>
-                    <div id=carte1><img src='./CartesAJouer/0_3.png'></div>
-                    <div id=carte2><img src='./CartesAJouer/0_3.png'></div>
-                    <div id=carte3><img src='./CartesAJouer/0_3.png'></div>
-                    <div id=carte4><img src='./CartesAJouer/0_3.png'></div>
-                    <div id=carte5><img src='./CartesAJouer/0_3.png'></div>
+                    <div id=carte1><img src='./ImagesCartes/0_3.png'></div>
+                    <div id=carte2><img src='./ImagesCartes/0_3.png'></div>
+                    <div id=carte3><img src='./ImagesCartes/0_3.png'></div>
+                    <div id=carte4><img src='./ImagesCartes/0_3.png'></div>
+                    <div id=carte5><img src='./ImagesCartes/0_3.png'></div>
                 </div>";
         ?>    
     </body>
