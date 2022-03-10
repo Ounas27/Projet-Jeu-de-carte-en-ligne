8343de5d2d32178a9172690cabffed33e6da7514
Ounas27 <73233838+Ounas27@users.noreply.github.com>
2022-03-10 08:46:16 +0100

    Add files via upload

---
+++ projet/accueil.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <link href="style.css" rel="stylesheet">
+    
+    <title>Projet Site 2022</title>
+</head>
+    <body>
+        <?php
+            echo"<div id=setJeux1>
+                    <div id=carte1><img src='./CartesAJouer/1_11.png'></div>
+                    <div id=carte2><img src='./CartesAJouer/2_11.png'></div>
+                    <div id=carte3><img src='./CartesAJouer/3_11.png'></div>
+                    <div id=carte4><img src='./CartesAJouer/4_11.png'></div>
+                    <div id=carte5><img src='./CartesAJouer/1_9.png'></div>
+                </div>
+
+                <div id=setJeux2>
+                    <div id=carte1><img src='./CartesAJouer/0_3.png'></div>
+                    <div id=carte2><img src='./CartesAJouer/0_3.png'></div>
+                    <div id=carte3><img src='./CartesAJouer/0_3.png'></div>
+                    <div id=carte4><img src='./CartesAJouer/0_3.png'></div>
+                    <div id=carte5><img src='./CartesAJouer/0_3.png'></div>
+                </div>
+
+                <div id=setJeux3>
+                    <div id=carte1><img src='./CartesAJouer/0_3.png'></div>
+                    <div id=carte2><img src='./CartesAJouer/0_3.png'></div>
+                    <div id=carte3><img src='./CartesAJouer/0_3.png'></div>
+                    <div id=carte4><img src='./CartesAJouer/0_3.png'></div>
+                    <div id=carte5><img src='./CartesAJouer/0_3.png'></div>
+                </div>
+
+                <div id=setJeux4>
+                    <div id=carte1><img src='./CartesAJouer/0_3.png'></div>
+                    <div id=carte2><img src='./CartesAJouer/0_3.png'></div>
+                    <div id=carte3><img src='./CartesAJouer/0_3.png'></div>
+                    <div id=carte4><img src='./CartesAJouer/0_3.png'></div>
+                    <div id=carte5><img src='./CartesAJouer/0_3.png'></div>
+                </div>";
+        ?>    
+    </body>
+
+</html>
+
